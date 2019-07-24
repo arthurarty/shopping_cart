1183a7bd2107f5ff4c0e8e3fd130e6b1cbd2babb
Arthur Nangai <arthurarty@users.noreply.github.com>
2019-07-24 22:17:47 +0300

    ft(database) - Create tables. (#1)
    - Create user table.
    - Create product table.
    - Create cart table.
    - Create table to track many to many relationship cart and product.

--- run.py
+++ run.py
@@ -1,11 +1,41 @@
-from flask import Flask, jsonify
+import re
+
+from collections import namedtuple
+from datetime import datetime
+
+from flask import Flask, jsonify, request
+from werkzeug.security import generate_password_hash
+
+from db.connection import Connection
+from modals.model import Model
 
 app = Flask(__name__)
+db = Connection.instance()
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return jsonify('Hello world')
+@app.route('/signup', methods=['POST'])
+def signup():
+    """Registering a new user"""
+    request_data = request.get_json()
+    email = request_data['email']
+    reg = re.match(
+        '([a-zA-Z0-9]+)([.]*)([-]*)([a-zA-Z0-9]+)@{1}([a-z]+)\.([a-z]+)',
+        email)
+    if reg is None:
+        return jsonify(message="Invalid email address"), 401
+    password = request_data['password']
+    # specify columns to insert data into
+    Columns = namedtuple('Columns', 'email password date_created')
+    new_user = Columns(
+        email=email,
+        password=generate_password_hash(password),
+        date_created=str(datetime.now())
+        )
+    user_table = Model(db, 'user_table')
+    user_table.create(new_user)
+    return jsonify(
+        message="Registration Successful"
+        ), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
