16b8871b93dd2974e142458057ff1a2f03f81823
arthurarty <arthur.nangai@gmail.com>
2019-07-24 20:43:55 +0300

    add flask

---
+++ run.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify('Hello world')
+
+if __name__ == '__main__':
+    app.run(debug=True)
