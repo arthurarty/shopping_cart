e39bee371a80a5608a2582e618e17a8f8d602562
arthurarty <arthur.nangai@gmail.com>
2019-07-24 21:35:48 +0300

    ft(database) - Create tables.
    - Create user table.
    - Create product table.
    - Create cart table.
    - Create table to track many to many relationship cart and product.

--- db/create_tables.py
+++ db/create_tables.py
@@ -3,10 +3,41 @@ from connection import Connection, Table
 # create db connection
 db = Connection()
 
-# create members table
-members_table = Table(db, 'company', '''
-      ID INT PRIMARY KEY     NOT NULL,
-      NAME           TEXT    NOT NULL,
-      AGE            INT     NOT NULL,
-      ADDRESS        CHAR(50),
-      SALARY         REAL''')
+# create users' table
+users_table = Table(db, 'user_table', '''
+      ID SERIAL PRIMARY KEY,
+      EMAIL          TEXT    NOT NULL UNIQUE,
+      PASSWORD       TEXT    NOT NULL,
+      DATE_CREATED   TIMESTAMP    NOT NULL
+      ''')
+
+# create product table
+product_table = Table(db, 'product', '''
+      ID SERIAL PRIMARY KEY,
+      NAME           VARCHAR(255)    NOT NULL,
+      PRICE          INT    NOT NULL,
+      DATE_CREATED   TIMESTAMP    NOT NULL,
+      UNIQUE (NAME, PRICE)
+      ''')
+
+# create cart table
+cart_table = Table(db, 'cart', '''
+      ID SERIAL PRIMARY KEY,
+      USER_ID INT,
+      FOREIGN KEY (USER_ID)
+      REFERENCES user_table (ID)
+      ON DELETE CASCADE
+      ''')
+
+# table to track cart - product many to many relationship
+cart_product_table = Table(db, 'cart_product', '''
+      ID SERIAL PRIMARY KEY,
+      PRODUCT_ID INT,
+      FOREIGN KEY (PRODUCT_ID)
+      REFERENCES product (ID)
+      ON DELETE CASCADE,
+      CART_ID INT,
+      FOREIGN KEY (CART_ID)
+      REFERENCES cart (ID)
+      ON DELETE CASCADE
+      ''')
