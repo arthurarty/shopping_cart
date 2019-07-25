cf667acee50c86c545df2b1a2cd58e4dc28880d6
Arthur Nangai <arthurarty@users.noreply.github.com>
2019-07-25 10:49:51 +0300

    feat(products) - Add CRUD functionality. (#4)
    - Register product blueprint.
    - Create product model.
    - Add CRUD functionality.

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,11 @@ from flask_jwt_extended import JWTManager
 def create_app():
     """Use app factory to create flask application"""
     app = Flask(__name__)
-
-    from .views import auth
-    app.register_blueprint(auth.bp)
     app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
     jwt = JWTManager(app)
+
+    from .views import auth, product
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(product.bp)
 
     return app
